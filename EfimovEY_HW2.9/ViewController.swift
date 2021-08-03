1c3c04e83b76f7846509a284fc74fc24bbb9ea71
EvgeniiEfimov <82396606+EvgeniiEfimov@users.noreply.github.com>
2021-08-03 16:09:06 +0300

    Initial Commit

---
+++ EfimovEY_HW2.9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EfimovEY_HW2.9
+//
+//  Created by User on 03.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
